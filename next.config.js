e1edf9d9072912bb0ccacc2235fafed3494d70b5
Hamza Akhtar <45310075+HamzaAkhtar090@users.noreply.github.com>
2026-01-15 01:08:35 +0500

    Update next.config.js for repo name and paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const repoName = "blue-guard-billing";
+
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
+  output: "export",
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+
   images: {
     unoptimized: true,
   },
